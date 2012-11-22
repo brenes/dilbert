5cef1c909be46d3319c28ac84365a1eb88263e1d
David J. Brenes <davidjbrenes@gmail.com>
2012-11-22 23:38:36 +0100

    First command example (it just prints the date)

---
+++ bin/dilbert.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'commander/import'
+
+program :version, "1"
+program :description, 'Fetch Dilbert strips and just enjoy'
+
+default_command :fetch
+
+command :fetch do |c|
+  c.syntax = 'dilbert fetch [options]'
+  c.summary = 'It just fetches dilbert strips!'
+  c.description = 'Say a date and I give you Dilbert'
+  c.example 'Fetching Dilbert strip from today', "dilbert fetch"
+  c.example 'Fetching Dilbert strip from yesterday', "dilbert fetch #{Date.today-1}"
+  c.action do |args, options|
+    puts "http://www.dilbert.com/strips/comic/#{args.first}"
+  end
+end
+
