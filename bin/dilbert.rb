3aab4d3c3c4ba76d8257d52d82fe225061094eef
David J. Brenes <davidjbrenes@gmail.com>
2012-11-22 23:52:10 +0100

    Parsing the dilbert strip page (with nokogiri) and getting the strip image url

--- bin/dilbert.rb
+++ bin/dilbert.rb
@@ -2,6 +2,8 @@
 
 require 'rubygems'
 require 'commander/import'
+require 'nokogiri'
+require 'open-uri'
 
 program :version, "1"
 program :description, 'Fetch Dilbert strips and just enjoy'
@@ -14,8 +16,18 @@ command :fetch do |c|
   c.description = 'Say a date and I give you Dilbert'
   c.example 'Fetching Dilbert strip from today', "dilbert fetch"
   c.example 'Fetching Dilbert strip from yesterday', "dilbert fetch #{Date.today-1}"
+
   c.action do |args, options|
-    puts "http://www.dilbert.com/strips/comic/#{args.first}"
+
+    date = Date.parse(args.first) rescue Date.today
+    strip_url = "http://www.dilbert.com/strips/comic/#{date}"
+    puts "Looking at #{strip_url}"
+
+    strip = Nokogiri::HTML(open(strip_url))
+    strip_img = strip.css('.STR_Image img').first
+    strip_img_url = "http://www.dilbert.com/#{strip_img.attr('src')}"
+
+    puts "Hoorray!! The strip is at #{strip_img_url}"
   end
 end
 
